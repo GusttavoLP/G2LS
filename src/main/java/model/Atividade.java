7b2924b8ee006c944277ac9bb77b3ab881d5a1ff
Gusttavo Locatelli <gusttavolocatelliportela@id.uff.br>
2024-07-02 20:21:48 -0300

    criar classes faltando

--- src/main/java/model/Atividade.java
+++ src/main/java/model/Atividade.java
@@ -2,24 +2,32 @@ package model;
 
 import lombok.Data;
 import javax.persistence.*;
+
+import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Data
-public class Espaco {
+public class Atividade {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
+    private String tipo;
     private String descricao;
     private Date data;
+    private Time horarioInicial;
+    private Time horarioFinal;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "espaco_id")
     private Espaco espaco;
 
     @ManyToMany(mappedBy = "atividades")
     private List<Usuario> usuarios;
+
+    @OneToMany(mappedBy = "atividades")
+    private List<Edicao> edicao;
 }
